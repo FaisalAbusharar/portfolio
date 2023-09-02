3db3f7a7cbfbdbd2be0f0e4cb67d155344ccc461
Voidy Devleoper <voidycodes@gmail.com>
2023-09-02 22:03:55 +0300

    slight progress

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import './App.css'; // Import your CSS file
-import background from "./images/second.png";
+import './css/App.css'; // Import your CSS file
+import NavigationBar from './NavigationBar';
 
 function App() {
-  const [showIcons, setShowIcons] = useState(false);
+  const [showInfo, setShowInfo] = useState(false);
 
   // Use useEffect to trigger the animation when the component mounts
   useEffect(() => {
     // Add a slight delay to give the page time to render
     setTimeout(() => {
-      setShowIcons(true);
+      setShowInfo(true);
     }, 500); // Adjust the delay time as needed
   }, []);
 
   return (
-    <body>
-      <img id="img" src={background} alt="background"></img>
+    <div className="background-container">
+      {/* <NavigationBar /> */}
       <div id="min-div">
-        <h1 id="title-name" className={showIcons ? 'move-up' : ''}>
-          FAISAL ANAS
+        <h1 id="title-name" className={showInfo ? 'move-up' : ''}>
+          Yo, I'm Faisal
         </h1>
-        {showIcons && (
-          <div className="social-icons">
-            {/* Replace the logos with hyperlinks */}
-            <a href="https://github.com/VoidyCD?tab=repositories" target="_blank" rel="noopener noreferrer">My Github</a>
-            <a href="https://www.youtube.com/channel/UC4ZEJ6S7L__oWlt5v5jiFMg" target="_blank" rel="noopener noreferrer">My Youtube</a>
-          </div>
-        )}
+        <div id="subtitle" className={showInfo ? 'move-up' : ''}>
+          Software Developer IN Python / C++
+        </div>
       </div>
-    </body>
+    </div>
   );
 }
 
 export default App;
+
---
+++ src/NavigationBar.js
@@ -0,0 +1,28 @@
+// NavigationBar.js
+
+import React from 'react';
+import './css/NavigationBar.css'; // Import your CSS file for styling
+import logo from "./images/mylogo.png"; // Import the logo image
+
+function NavigationBar() {
+  return (
+    <header className="navbar">
+      <div className="logo">
+        {/* Use the imported logo as the src */}
+        <h2>FAISAL</h2>
+      </div>
+      <div className="title">
+        <h1>SOFTWARE DEVELOPER</h1>
+      </div>
+      <nav>
+        <ul className="nav-links">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#work">Work</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default NavigationBar;
